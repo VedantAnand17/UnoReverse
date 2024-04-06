c99ba9529c496ce511bbeaa15c130244adc11ec0
VedantAnand17 <vedantanand0077@gmail.com>
2024-04-07 04:09:36 +0530

    .

--- fetch.js
+++ fetch.js
@@ -1,17 +1,14 @@
 // main.js
-async function fetchNumber() {
+document.getElementById('percentageElement').addEventListener('click', async () => {
     try {
         const response = await fetch('http://localhost:3001/get'); // Fetch the number from the server
         const data = await response.json();
         const randomNumber = data.number;
 
-        // Set the data-percent attribute
+        // Set the data-percent attribute in elementProgress.html
         const percentageElement = document.getElementById('percentageElement');
         percentageElement.setAttribute('data-percent', randomNumber);
     } catch (error) {
         console.error('Error fetching number:', error);
     }
-}
-
-// Call the function when the page loads
-fetchNumber();
+});
