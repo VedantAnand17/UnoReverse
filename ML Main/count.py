1f90a7d912530f594af436c144fa5e290655e39d
Dhwanigoyal <139227134+Dhwanigoyal@users.noreply.github.com>
2024-04-07 03:52:11 +0530

    nahi hora bc

--- ML Main/count.py
+++ ML Main/count.py
@@ -1,6 +1,8 @@
 import pandas as pd
-df = pd.read_excel('traning.xlsx')
-
-for i in range(60):
-    count  = 0
-    
+df = pd.read_excel('ML Main/traning.xlsx')
+counts = []
+disc = df.iloc[2]
+print(disc)
+# for i in range(60):
+#     count  = 0
+#     for x in df.iloc[i]    
