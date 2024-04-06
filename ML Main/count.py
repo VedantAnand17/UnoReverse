b68c02cdd577bc3d061579e62fa90d112f93d344
thebrownkidd <139674706+thebrownkidd@users.noreply.github.com>
2024-04-07 03:59:41 +0530

    added counter in train set

--- ML Main/count.py
+++ ML Main/count.py
@@ -1,8 +1,19 @@
 import pandas as pd
+import json
 df = pd.read_excel('ML Main/traning.xlsx')
 counts = []
-disc = df.iloc[2]
-print(disc)
-# for i in range(60):
-#     count  = 0
-#     for x in df.iloc[i]    
+disc = df.iloc[:,1]
+# for x in disc:
+#     print(x)
+f = open('test1.json')
+dic = json.load(f)
+for x in disc:
+    count = 0
+    for y in x:
+        y = y.lower()
+        if y in dic.keys():
+            count += dic[y]
+    counts.append(count)
+
+for x in counts:
+    print(x)
